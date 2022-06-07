4a0c1d7b9cc5d2d4b6ac7854fe3111c7bbaba25c
Xingong Li <lixi@ku.edu>
2022-06-07 12:52:22 -0500

    Add random stage test

--- apps/flood_map.py
+++ apps/flood_map.py
@@ -33,21 +33,24 @@ def app():
     # print(leafmap.cog_validate(url))
 
     # create a leafmap Map and center it to east Kansas
-    leafmap.Map()
+    # leafmap.Map()
     m = leafmap.Map(center=(38.587981, -96.815613),zoom = 8, locate_control=True) # breaks at level 7
+    # add Google Terrain as a basemap
     m.add_basemap("TERRAIN")
 
-    # add basemap
-    fldplnUrl = 'https://services.kars.geoplatform.ku.edu/arcgis/services/fldpln_kansas/Basemap/MapServer/WMSServer?'
+    # add FLDPLN basemaps (LIDAR terrain hillshade, floodplains, streams, ...)
+    fldplnBasemapUrl = 'https://services.kars.geoplatform.ku.edu/arcgis/services/fldpln_kansas/Basemap/MapServer/WMSServer?'
+    # add floodplains and strems (i.e., flood source pixels) layers. Layers added later are on the top!
+    m.add_wms_layer(url=fldplnBasemapUrl, layers='0', name='Floodplains', format='image/png', transparent=True, shown=True) # But this layer is layer 1 on MapServer!
+    m.add_wms_layer(url=fldplnBasemapUrl, layers='1', name='Streams', format='image/png', transparent=True, shown=True)
 
-    # add forecast layers, layers added later are on the top!
-    m.add_wms_layer(url=fldplnUrl, layers='0', name='Floodplains', format='image/png', transparent=True, shown=True) # But this layer is layer 1 on MapServer!
-    m.add_wms_layer(url=fldplnUrl, layers='1', name='Streams', format='image/png', transparent=True, shown=True)
-
+    # add additional FLDPLN reference layers (MinDtf, flood category layes (Action, Flood, Moderate, and Major))
     mindtfUrl = 'https://services.kars.geoplatform.ku.edu/arcgis/services/fldpln_kansas/MinDtf/ImageServer/WMSServer'
     m.add_wms_layer(url=mindtfUrl, layers='0', name='mindtf', format='image/png', transparent=True, shown=True)
 
-    # add flood maps
+    # add current and 14-day max flood maps
+    testUrl = 'https://services.kars.geoplatform.ku.edu/arcgis/services/fldpln_kansas/ConstantStage/ImageServer/WMSServer'
+    m.add_wms_layer(url=testUrl, layers='0', name='Random Constant Stage', format='image/png', transparent=True, shown=True)
     # m.add_cog_layer('https://fldpln.blob.core.windows.net/maps/spring_MinDtf.tif', name="Minimum Depth-to-Flood", palette="reds",shown=False)
     # m.add_cog_layer('https://fldpln.blob.core.windows.net/maps/spring_Major.tif', name="Major flood map", palette="reds")
     # m.add_cog_layer('https://fldpln.blob.core.windows.net/maps/spring_Action.tif', name="Action flood map", palette="reds")
